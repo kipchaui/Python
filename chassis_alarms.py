0dfe0d92464b5cecaec75d3fd3a9a0132cfded88
kipchaui <ekipchaui@gmail.com>
2021-01-14 02:20:30 -0600

    python script to check chassis alarms on Juniper devices

---
+++ chassis_alarms.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+from netmiko import ConnectHandler
+import json
+import sys
+import netmiko
+
+# Defines Netmiko exceptions
+netmiko_exceptions = (netmiko.ssh_exception.NetmikoTimeoutException,
+                        netmiko.ssh_exception.NetmikoAuthenticationException)
+
+# Defines the command you must run, otherwise code exits
+if len(sys.argv) < 2:
+        print('Usage: chassis_alarms.py routers.json')
+        exit()
+
+# Load list of routers from routers.json file
+with open(sys.argv[1]) as dev_file:
+        routers = json.load(dev_file)
+
+# Create a txt file and open with write priviledges
+filename = 'show_chassis_alarms.txt'
+with open(filename, 'w') as output_file:
+        # Login to each device with provided logins
+        for routers in routers:
+                try:
+                        net_connect = ConnectHandler(**routers)
+
+                        # Print tilde 39 times, then print the hostname then print tilde another 39 times
+                        output_file.write('~' * 39 + net_connect.base_prompt + '~' * 39 + '\n\n')
+
+                        # Runs "show chassis alarms" command and store output on the created file
+                        output_file.write('## Output of: show chassis alarms' '\n')
+                        output_file.write(net_connect.send_command('show chassis alarms') + '\n')
+                except netmiko_exceptions as e:
+                        output_file.write('~' * 37 + 'Failed to connect to: ' + '~' * 37 + '\n\n'.format(str(routers), str(e)))
+                finally:
+                        pass
